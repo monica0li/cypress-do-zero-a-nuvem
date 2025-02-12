f0535914be3055fe41fc017ea61858861c6348f9
“Monica <monicaoliv0805@gmail.com>
2025-02-11 23:09:29 -0300

    execicios 5 a 7

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,12 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
 
+  const user = {
+    name: 'Joaozinho',
+    lastName: 'Daa Silva',
+    email: 'joaozinho@email.com',
+    text: Cypress._.repeat('texto ', 10)
+  }
+
   beforeEach(() => {
     cy.visit('./src/index.html')
   })
@@ -7,41 +14,91 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
-
+// exc extra 1
   it('preenche os campos origatórios e envia o formulário', () => {
     cy.get('#firstName').type('nome')
     cy.get('#lastName').type('sobrenome')
     cy.get('#email').type('email@email.com')
     cy.get('#open-text-area').type('lalalaallal', {delay: 10})
-    cy.get('button').click()
+    cy.contains('Enviar').click()
     cy.get('.success').should('be.visible')
   })
-
+// exc extra 2
   it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
     cy.get('#firstName').type('nome')
     cy.get('#lastName').type('sobrenome')
     cy.get('#email').type('email')
     cy.get('#open-text-area').type('lalalaallal', {delay: 10})
-    cy.get('button').click()
+    cy.contains('Enviar').click()
     cy.get('.error').should('be.visible')
   })
-
+// exc extra 3
   it('campo de telefone permanece vazio ao digitar valores não-numéricos', () => {
-    cy.get('#phone').type('a?/;@#$%!%¨&*()_+}?{`^:').should('have.value', '')
+    cy.get('#phone')
+      .type('a?/;@#$%!%¨&*()_+}?{`^:')
+      .should('have.value', '')
   })
-
+// exc extra 4
   it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', ()=> {
     cy.get('#firstName').type('nome')
     cy.get('#lastName').type('sobrenome')
     cy.get('#email').type('email@email.com')
     cy.get('#open-text-area').type('lalalaallal', {delay: 10})
     cy.get('#phone-checkbox').click()
-    cy.get('button').click()
+    cy.contains('Enviar').click()
+
     cy.get('.error').should('be.visible')
   })
+// exc extra 5
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('nome')
+      .should('have.value', 'nome')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('sobrenome')
+      .should('have.value','sobrenome')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('email@email.com')
+      .should('have.value', 'email@email.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#open-text-area')
+      .type('lalalaallal', {delay: 10})
+      .should('have.value', 'lalalaallal')
+      .clear()
+      .should('have.value', '')
 
+    
+  })
+// exc extra 6
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios',() => {
     cy.get('button').click()
+
     cy.get('.error').should('be.visible')
+  })
+// exc extra 7.1
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+
+  })
+// exc extra 7.2
+  it('envia o form com comando customizado, mas usando parametro', () => {
+    cy.fillMandatoryFieldsAndSubmitUsingAObject(user)
+
+    cy.get('.success').should('be.visible')
+
+  })
+
+  it('envia o form com comando customizado, mas usando parametro e valor padrao', () => {
+    cy.fillMandatoryFieldsAndSubmitUsingAObjectDefault()
+
+    cy.get('.success').should('be.visible')
+
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+    cy.get('#firstName').type('nome')
+    cy.get('#lastName').type('sobrenome')
+    cy.get('#email').type('email@email.com')
+    cy.get('#open-text-area').type('lalalaallal', {delay: 10})
+    cy.contains('Enviar').click()
+})
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmitUsingAObject', user => {
+    cy.get('#firstName').type(user.name)
+    cy.get('#lastName').type(user.lastName)
+    cy.get('#email').type(user.email)
+    cy.get('#open-text-area').type(user.text)
+    cy.contains('Enviar').click()
+})
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmitUsingAObjectDefault', (user = {
+    name: 'Nome',
+    lastName: 'Sobrenome',
+    email: 'email@email.com',
+    text: 'lalalaallal'
+}) => {
+    cy.get('#firstName').type(user.name)
+    cy.get('#lastName').type(user.lastName)
+    cy.get('#email').type(user.email)
+    cy.get('#open-text-area').type(user.text)
+    cy.contains('Enviar').click()
+})
